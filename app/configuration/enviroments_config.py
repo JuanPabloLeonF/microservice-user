3b1675bfa9820314fb3656e9874932e496aff1ad
Juan Pablo Leon <fonseca123papo@gmail.com>
2025-03-16 23:04:06 -0500

    corrigiendo las variables de entonro por que no utilzo algunas

--- app/configuration/enviroments_config.py
+++ app/configuration/enviroments_config.py
@@ -5,10 +5,7 @@ PORT: int = int(os.getenv("PORT", "2000"))
 DEBUG: bool = os.getenv("DEBUG", "False") == "True"
 
 VERSION: str = os.getenv("VERSION", "1.0.0")
-TITLE: str = os.getenv("TITLE", "API_GATEWAY")
-SECRET_KEY_JWT: str = os.getenv("SECRET_KEY_JWT", "secret_key")
-ALGORITHM_JWT: str = os.getenv("ALGORITHM_JWT", "HS256")
-EXPIRATION_TIME_TOKEN_MINUTES_JWT: int = int(os.getenv("EXPIRATION_TIME_TOKEN_MINUTES_JWT", "15"))
+TITLE: str = os.getenv("TITLE", "MICROSERVICE-USER")
 
 DATABASE_URL: str = os.getenv("DATABASE_URL", "")
 DATABASE_URL_DEV: str = os.getenv("DATABASE_URL_DEV", "")
